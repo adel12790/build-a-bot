48d8d7c64decdb04d1b26e716c9a82814f9c2892
Mohamed Adel <madel@vrtron.com>
2020-05-18 12:21:07 +0200

    - testing out the class binding - trying mixins and hooks

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,46 +2,48 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to cart</button>
     <div class="top-row">
-      <div class="top part">
-        <div class="robot-name">{{selectedRobot.head.title}}
-          <span v-if="selectedRobot.head.onSale" class="sale">Sale!!</span></div>
-        <img :src="selectedRobot.head.src" title="head"/>
+      <div :class="[saleBorderClass, 'top', 'part']">
+        <div class="robot-name">
+          {{selectedRobot.head.title}}
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale!!</span>
+        </div>
+        <img :src="selectedRobot.head.src" title="head" />
         <button @click="selectPreviousPart('heads')" class="prev-selector">&#9668;</button>
         <button @click="selectNextPart('heads')" class="next-selector">&#9658;</button>
       </div>
     </div>
     <div class="middle-row">
       <div class="left part">
-        <img :src="selectedRobot.arms.src" title="left arm"/>
+        <img :src="selectedRobot.arms.src" title="left arm" />
         <button @click="selectPreviousPart('arms')" class="prev-selector">&#9650;</button>
         <button @click="selectNextPart('arms')" class="next-selector">&#9660;</button>
       </div>
       <div class="center part">
-        <img :src="selectedRobot.torso.src" title="left arm"/>
+        <img :src="selectedRobot.torso.src" title="left arm" />
         <button @click="selectPreviousPart('torsos')" class="prev-selector">&#9668;</button>
         <button @click="selectNextPart('torsos')" class="next-selector">&#9658;</button>
       </div>
       <div class="right part">
-        <img :src="selectedRobot.arms.src" title="left arm"/>
+        <img :src="selectedRobot.arms.src" title="left arm" />
         <button @click="selectPreviousPart('arms')" class="prev-selector">&#9650;</button>
         <button @click="selectNextPart('arms')" class="next-selector">&#9660;</button>
       </div>
     </div>
     <div class="bottom-row">
       <div class="bottom part">
-        <img :src="selectedRobot.base.src" title="left arm"/>
+        <img :src="selectedRobot.base.src" title="left arm" />
         <button @click="selectPreviousPart('bases')" class="prev-selector">&#9668;</button>
         <button @click="selectNextPart('bases')" class="next-selector">&#9658;</button>
       </div>
     </div>
-    <div>
+    <div class="cart">
       <h1>Cart</h1>
       <table>
         <thead>
-        <tr>
-          <th>Robot</th>
-          <th class="cost"> Cost</th>
-        </tr>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
         </thead>
         <tbody>
           <tr v-for="(robot, index) in cart" :key="index">
@@ -56,6 +58,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const depricatedIndex = index - 1;
@@ -68,6 +71,7 @@ function getNextValidIndex(index, length) {
 }
 export default {
   name: 'RobotBuilder',
+
   data() {
     return {
       availableParts,
@@ -80,7 +84,14 @@ export default {
       },
     };
   },
+  mixins: createdHookMixin,
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
+    headBorderStyle() {
+      return { border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid gray' };
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedPartIndex.heads],
@@ -103,33 +114,35 @@ export default {
     },
     addToCart() {
       const robot = this.selectedRobot;
-      const cost = robot.heads.cost + robot.arms.cost + robot.torsos.cost + robot.bases.cost;
+      const cost = robot.head.cost + robot.arms.cost + robot.torso.cost + robot.base.cost;
       this.cart.push({ ...robot, cost });
     },
   },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .part {
   position: relative;
-  width:165px;
-  height:165px;
+  width: 165px;
+  height: 165px;
   border: 3px solid #aaa;
 }
-.part img {
-  width:165px;
+.part {
+  img {
+    width: 165px;
+  }
 }
 .top-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
 }
 .middle-row {
-  display:flex;
+  display: flex;
   justify-content: center;
 }
 .bottom-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
   border-top: none;
 }
@@ -153,7 +166,7 @@ export default {
 }
 .prev-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   left: -28px;
   width: 25px;
@@ -161,14 +174,15 @@ export default {
 }
 .next-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   right: -28px;
   width: 25px;
   height: 171px;
 }
-.center .prev-selector, .center .next-selector {
-  opacity:0.8;
+.center .prev-selector,
+.center .next-selector {
+  opacity: 0.8;
 }
 .left .prev-selector {
   top: -28px;
@@ -199,31 +213,38 @@ export default {
 .right .next-selector {
   right: -3px;
 }
-.robot-name{
+.robot-name {
   position: absolute;
   top: -25px;
   text-align: center;
   width: 100%;
 }
-.sale{
+.sale {
   color: red;
 }
-.content{
+.content {
   position: relative;
 }
-.add-to-cart{
-  position:absolute;
-  right:30px;
+.add-to-cart {
+  position: absolute;
+  right: 30px;
   width: 120px;
   padding: 3px;
   font-size: 16px;
 }
-th,td{
+th,
+td {
   text-align: left;
   padding: 5px;
   padding-right: 20px;
 }
-.cost{
+.cost {
   text-align: right;
+}
+.cart {
+  text-align: left;
+}
+.sale-border {
+  border: 3px solid red;
 }
 </style>
